bb89ceecae4a01a54c73ac7f22c868ed8cab63a9
Mannan-28 <mannan.singh@students.asdrp.org>
2026-07-14 13:44:41 -0700

    5,6 done

--- game.py
+++ game.py
@@ -1,32 +1,37 @@
-import random
-
-#1, 2
-word_bank = ['fruits', 'pineapple', 'smoothie', 'jambajuice', 'apples', 'strawberry', 'grapes', 'lemonade', 'bananas', 'oranges', 'peaches']
-secret_word = random.choice(word_bank)
-lives = 6
-user_input = null
-
-print("Let’s play hangman!")
-
-
-
-#9, 10, 11
-if lives == 0 or user_input == secret_word:
-    if lives == 0:
-        print ("Out of Lives! Game Over!")
-    if user_input == word:
-        print ("You win!")   
-    response = input ("Play Again? (yes/no)")
+    import random
+    
+    #1, 2
+    word_bank = ['fruits', 'pineapple', 'smoothie', 'jambajuice', 'apples', 'strawberry', 'grapes', 'lemonade', 'bananas', 'oranges', 'peaches']
+    secret_word = random.choice(word_bank)
+    lives = 6
+    user_input = null
+    
+    print("Let’s play hangman!")
+    #5, 6
     while True:
-        if response != "yes" and response != "no":
-            response = input ("Invalid Input. Play Again? (yes/no)")
-        if response == "yes":
-            break 
-        if response == "no":
-            raise SystemExit()
-
-        
-
-
-        
-
+        user_input = input("Guess a letter: ")
+    
+        if len(user_input) == 1 and user_input.isalpha():
+            break
+    
+        print("Not a valid guess. Please enter a single letter.")
+    #9, 10, 11
+    if lives == 0 or user_input == secret_word:
+        if lives == 0:
+            print ("Out of Lives! Game Over!")
+        if user_input == word:
+            print ("You win!")   
+        response = input ("Play Again? (yes/no)")
+        while True:
+            if response != "yes" and response != "no":
+                response = input ("Invalid Input. Play Again? (yes/no)")
+            if response == "yes":
+                break 
+            if response == "no":
+                raise SystemExit()
+    
+            
+    
+    
+            
+    
