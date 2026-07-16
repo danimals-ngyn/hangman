e61f8d57f2cdf87dd4eb52694ae7d8a44b16d833
adiponangi <aditya.ponangi@students.asdrp.org>
2026-07-15 17:12:08 -0700

    fixed some syntax

--- game.py
+++ game.py
@@ -4,7 +4,7 @@
     word_bank = ['fruits', 'pineapple', 'smoothie', 'jambajuice', 'apples', 'strawberry', 'grapes', 'lemonade', 'bananas', 'oranges', 'peaches']
     secret_word = random.choice(word_bank)
     lives = 6
-    user_input = null
+    user_input = None
     
     print("Let’s play hangman!")
     #5, 6
@@ -16,17 +16,17 @@
     
         print("Not a valid guess. Please enter a single letter.")
         #7, 8
-if "_" not in display:
-    break
+    if "_" not in display:
+        break
 
-if lives <= 0:
-    print("Out of lives, game over!")
-    break
+    if lives <= 0:
+        print("Out of lives, game over!")
+        break
     #9, 10, 11
     if lives == 0 or user_input == secret_word:
         if lives == 0:
             print ("Out of Lives! Game Over!")
-        if user_input == word:
+        if user_input == secret_word:
             print ("You win!")   
         response = input ("Play Again? (yes/no)")
         while True:
