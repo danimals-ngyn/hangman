7f41bb8762a38a9180a2cc70f30849884694efb5
danimals-ngyn <dan.nguyen@students.asdrp.org>
2026-07-15 18:09:44 -0700

    Create final.py
    after debugging and writing 3+4

---
+++ final.py
@@ -0,0 +1,57 @@
+import random
+
+word_bank = ['fruits', 'pineapple', 'smoothie', 'jambajuice', 'apples', 'strawberry', 'grapes', 'lemonade', 'bananas', 'oranges', 'peaches'] #intializes usable wordbank
+secret_word = random.choice(word_bank) #chooses random word
+lives = 6 #default lives is 6
+valid = False #boolean to check if guess results in success or loss
+underscores = "" #intializes underscores as empty 
+
+for letter in secret_word: #sets up underscores
+    underscores = underscores + "_"
+    if letter != secret_word[-1]:
+        underscores = underscores + " " #adds space between underscores but not after the last one
+
+
+print("\nLet’s play hangman!") #introduction prompt
+
+while True: 
+    print (underscores) #print progress each time 
+    print ("Lives: " + str(lives)) #lives
+
+    guess = input("Guess?\n") #asks for user input
+    
+
+    if len(guess) == 1 and guess.isalpha: #checks input validaity  
+        for letter in secret_word: #goes through each character of the secret word and checks the guess against the character
+                if guess.lower() == letter: #lowercases the letter to account for both capital and lowercase inputs since case sensitive inputs are annoying
+                    underscores = underscores[:(secret_word.index(letter))*2] + letter + underscores[(secret_word.index(letter)*2)+1:] #splices the underscores based on the position of the letter in the secret word and replaces underscore with letter
+                    secret_word = secret_word[:secret_word.index(letter)] + "`" + secret_word[secret_word.index(letter)+1:] #changes the corresponding letter in the secret word to a "`" because secret_word.index(letter) only looks for the first instance of that letter in the string, ignoring words that have multiple instances of the same letter, and so replacing it allows consideration of other instances of the ltter in the for loop. using "'" because no one uses it and its not a valid input anyway.
+                    valid = True #if an instance of the gussed letter is found in the secret word, the guess is valid and no life is lost
+        if valid == False: #if an instance is not found after going through the whole word, life is lost.
+            print ("Letter Not Found!")
+            lives -=1
+        valid = False #resets to default as false
+    else:
+        print("Not a valid guess. Please enter a single letter.") #for when input is not a valid input.
+
+
+    if lives <= 0 or "_" not in underscores: #checks for winning/losing/endgame event after each guess (when lives are gone or no more underscores)
+        if lives <= 0:
+            print ("Out of Lives! Game Over!")
+        if "_" not in underscores:
+            print ("You win!")   
+        response = input ("Play Again? (yes/no)") #in either event (winning or losing), play again is prompted
+        while True:
+            if response != "yes" and response != "no":
+                response = input ("Invalid Input. Play Again? (yes/no)") #checks input is valid. 
+            if response == "yes":
+                lives = 6 #rests lives and the loop comes again. 
+                break 
+            if response == "no":    
+                raise SystemExit() #execute terminal if no
+    
+            
+    
+    
+            
+    
